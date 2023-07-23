d2233740c8f9626bd5768d2dde8186b11a7722af
Fazeel Jaffer <fazeeljaffer@gmail.com>
2023-07-23 10:39:44 +0530

    bckfjs

--- app.py
+++ app.py
@@ -1,36 +1,58 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-
 app = Flask(__name__)
-CORS(app)  # Enable CORS for all routes in the app
+CORS(app)
 
-# Your code for peptide extraction and probability score calculation goes here
-# You can use the Python functions you've written for this task
+@app.route('/')
+def home():
+    return "Server is Listening"
 
-@app.route('/predict', methods=['POST'])
+
+@app.route('/predict', methods=['GET', 'POST'])
 def predict():
-    try:
-        protein_sequence = request.form['protein_sequence']
+    data = request.get_json()
+    protein_sequence = data.get('protein_sequence')
+    threshold = float(data.get('threshold'))
+    enable_threshold = data.get('enableThreshold')
 
-        # Call your functions for peptide extraction and probability score calculation
-        # Replace the following code with your actual logic
-        peptides = ["Peptide1", "Peptide2", "Peptide3"]
-        scores = [0.75, 0.86, 0.92]
+    print(protein_sequence, threshold, enable_threshold)
 
-        # Prepare the response data
-        data = []
-        for i in range(len(peptides)):
-            data.append({
-                'sequence_id': f'Sequence {i+1}',
-                'peptide_sequence': peptides[i],
-                'methylation_site': 'R',
-                'score': scores[i]
-            })
+    # Process the protein_sequence and make predictions
+    # ...
+    #Q92997	27	LVKLPLPAERVTLADFKGV	0.598706
+    prediction_results = [{"SeqId":"Q92997","R site":27,"Peptides":"LVKLPLPAERVTLADFKGV","Prediction Score":0.598706}
+                          ,{"SeqId":"Q92997","R site":27,"Peptides":"LVKLPLPAERVTLADFKGV","Prediction Score":0.598706},
+                          {"SeqId":"Q92997","R site":27,"Peptides":"LVKLPLPAERVTLADFKGV","Prediction Score":0.598706},
+                          {"SeqId":"Q92997","R site":27,"Peptides":"LVKLPLPAERVTLADFKGV","Prediction Score":0.598706},
+                          {"SeqId":"Q92997","R site":27,"Peptides":"LVKLPLPAERVTLADFKGV","Prediction Score":0.598706},
+                          {"SeqId":"Q92997","R site":27,"Peptides":"LVKLPLPAERVTLADFKGV","Prediction Score":0.598706},
+                          {"SeqId":"Q92997","R site":27,"Peptides":"LVKLPLPAERVTLADFKGV","Prediction Score":0.598706},
+                          {"SeqId":"Q92997","R site":27,"Peptides":"LVKLPLPAERVTLADFKGV","Prediction Score":0.598706}
+                          ,{"SeqId":"Q92997","R site":27,"Peptides":"LVKLPLPAERVTLADFKGV","Prediction Score":0.598706},
+                          {"SeqId":"Q92997","R site":27,"Peptides":"LVKLPLPAERVTLADFKGV","Prediction Score":0.598706},
+                          {"SeqId":"Q92997","R site":27,"Peptides":"LVKLPLPAERVTLADFKGV","Prediction Score":0.598706},
+                          {"SeqId":"Q92997","R site":27,"Peptides":"LVKLPLPAERVTLADFKGV","Prediction Score":0.598706},
+                          {"SeqId":"Q92997","R site":27,"Peptides":"LVKLPLPAERVTLADFKGV","Prediction Score":0.598706},
+                          {"SeqId":"Q92997","R site":27,"Peptides":"LVKLPLPAERVTLADFKGV","Prediction Score":0.598706},
+                          {"SeqId":"Q92997","R site":27,"Peptides":"LVKLPLPAERVTLADFKGV","Prediction Score":0.598706}
+                          ,{"SeqId":"Q92997","R site":27,"Peptides":"LVKLPLPAERVTLADFKGV","Prediction Score":0.598706},
+                          {"SeqId":"Q92997","R site":27,"Peptides":"LVKLPLPAERVTLADFKGV","Prediction Score":0.598706},
+                          {"SeqId":"Q92997","R site":27,"Peptides":"LVKLPLPAERVTLADFKGV","Prediction Score":0.598706},
+                          {"SeqId":"Q92997","R site":27,"Peptides":"LVKLPLPAERVTLADFKGV","Prediction Score":0.598706},
+                          {"SeqId":"Q92997","R site":27,"Peptides":"LVKLPLPAERVTLADFKGV","Prediction Score":0.598706},
+                          {"SeqId":"Q92997","R site":27,"Peptides":"LVKLPLPAERVTLADFKGV","Prediction Score":0.598706},
+                          {"SeqId":"Q92997","R site":27,"Peptides":"LVKLPLPAERVTLADFKGV","Prediction Score":0.598706}
+                          ,{"SeqId":"Q92997","R site":27,"Peptides":"LVKLPLPAERVTLADFKGV","Prediction Score":0.598706},
+                          {"SeqId":"Q92997","R site":27,"Peptides":"LVKLPLPAERVTLADFKGV","Prediction Score":0.598706},
+                          {"SeqId":"Q92997","R site":27,"Peptides":"LVKLPLPAERVTLADFKGV","Prediction Score":0.598706},
+                          {"SeqId":"Q92997","R site":27,"Peptides":"LVKLPLPAERVTLADFKGV","Prediction Score":0.598706},
+                          {"SeqId":"Q92997","R site":27,"Peptides":"LVKLPLPAERVTLADFKGV","Prediction Score":0.598706},
+                          {"SeqId":"Q92997","R site":27,"Peptides":"LVKLPLPAERVTLADFKGV","Prediction Score":0.598706}
+                          ]
 
-        return jsonify(data), 200
+    # Return the prediction results as JSON
+    return jsonify(prediction_results)
 
-    except Exception as e:
-        return jsonify({'error': str(e)}), 400
 
 if __name__ == '__main__':
     app.run(debug=True)
